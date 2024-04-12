029ee522b78473c9c8e89e4a23ff3f304db2607b
alan <kno12350@gmail.com>
2024-04-12 14:48:27 +0900

    클래스의 toString()함수를 override 로 재정의 후 사용

--- bin/custom_class/value_equality.dart
+++ bin/custom_class/value_equality.dart
@@ -1,6 +1,14 @@
 class A {
   int value;
   A(this.value);
+
+
+  @override
+  // override 는 현재 클래스에서 사용할 수 있는 함수를 재정의하기 위해서 쓰며
+  String toString() {
+    // return 을 해당 함수 처럼 쓰면 리턴 겂으로 정확하게 값을 나타낼 수 있음
+    return "A($value)";
+  }
 }
 
 void main() {
